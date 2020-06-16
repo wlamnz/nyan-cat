8ddca83c1a88e9b25c638e33fe284aed9fd00915
William Lam <william.lam.nz@gmail.com>
2020-06-17 00:54:53 +1200

    Add bash script

---
+++ runcat.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+while :; 
+do
+ clear && cat nyancat1 | lolcat && sleep 0.07
+ clear && cat nyancat2 | lolcat && sleep 0.07
+ clear && cat nyancat3 | lolcat && sleep 0.07
+ clear && cat nyancat2 | lolcat && sleep 0.07
+done
