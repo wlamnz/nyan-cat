f3521e5d5e7a524ffd454fdd96366b62fa71511a
William Lam <william.lam.nz@gmail.com>
2020-06-17 00:58:02 +1200

    Add script that doesn't require lolcat

---
+++ runcat3.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+while :; 
+do
+ clear && cat nyancat1 && sleep 0.07
+ clear && cat nyancat2 && sleep 0.07
+ clear && cat nyancat3 && sleep 0.07
+ clear && cat nyancat2 && sleep 0.07
+done
