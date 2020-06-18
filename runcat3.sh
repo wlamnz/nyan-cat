edd61dcb7f64eff5ce6805075e3dce2707de9e49
William Lam <william.lam.nz@gmail.com>
2020-06-18 22:10:02 +1200

    Change speed of runcat3.sh

--- runcat3.sh
+++ runcat3.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+speed=0.08
+
 while :; 
 do
- clear && cat nyancat1 && sleep 0.07
- clear && cat nyancat2 && sleep 0.07
- clear && cat nyancat3 && sleep 0.07
- clear && cat nyancat2 && sleep 0.07
+ clear && cat nyancat1 && sleep $speed
+ clear && cat nyancat2 && sleep $speed
+ clear && cat nyancat3 && sleep $speed
+ clear && cat nyancat2 && sleep $speed
 done
